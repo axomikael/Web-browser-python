d761c9b1aab6a2d38644cdd2b67d7677f732bd25
axomikael <127490345+axomikael@users.noreply.github.com>
2023-03-12 17:03:08 +0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QToolBar, QAction, QTabWidget, QStatusBar
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtPrintSuppot import *
+from PyQt5.QtGui import *
